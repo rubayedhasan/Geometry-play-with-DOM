d39a11032cdc85d415c5ed42054431cc3bd3f43d
rubayedhasan <mdrubayedhasan2002@gmail.com>
2024-06-23 22:28:35 +0600

    adding line of code to calculate area of pentagon & ellipses

--- script/ellipses-area-calculation.js
+++ script/ellipses-area-calculation.js
@@ -1,1 +1,11 @@
-console.log("ellipses");
+function calculateEllipsesArea() {
+  // get major radius value
+  const a = getInputValue("ellipses-major-radius");
+
+  // get minor radius value
+  const b = getInputValue("ellipses-minor-radius");
+  const area = Math.PI * a * b;
+
+  //   display area
+  displayResult("ellipses-area-display", area);
+}
--- script/pentagon-area-calculation.js
+++ script/pentagon-area-calculation.js
@@ -1,1 +1,14 @@
-console.log("pentagon");
+function calculatePentagonArea() {
+  // get perimeter value
+  const perimeterInputField = document.getElementById("pentagon-perimeter");
+  const perimeterStringValue = perimeterInputField.value;
+  const perimeter = parseFloat(perimeterStringValue);
+
+  //   get apothem value
+  const apothem = parseFloat(document.getElementById("pentagon-apothem").value);
+
+  const area = 0.5 * perimeter * apothem;
+
+  //   display area
+  document.getElementById("pentagon-area-display").innerText = area;
+}
